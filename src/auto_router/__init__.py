00aab0971daaa2069374392e232ebbfdf1b79f46
Scott Joyner <kipnerter@gmail.com>
2026-08-06 10:45:33 -0400

    Install benchmark-aware routing policy

--- src/auto_router/__init__.py
+++ src/auto_router/__init__.py
@@ -2,9 +2,11 @@
 
 
 def _install_runtime_policies() -> None:
+    from .benchmark_routing_policy import install_benchmark_routing_policy
     from .discovery_enabled_policy import install_enabled_discovery_policy
 
     install_enabled_discovery_policy()
+    install_benchmark_routing_policy()
 
 
 _install_runtime_policies()
